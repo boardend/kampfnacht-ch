0e1ccc7e7d7ae62bc88668cb6a0bdb728929684b
gift <gift@gift.(none)>
2013-12-07 14:54:38 +0100

    restful welcome service

--- src/main/java/ch/kampfnacht/AppController.java
+++ src/main/java/ch/kampfnacht/AppController.java
@@ -5,17 +5,16 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
 @ComponentScan
 public class AppController {
-
+	
     @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
+    protected String redirect() 
+    {
+        return "redirect:app/index.html";
     }
 
     public static void main(String[] args) throws Exception {
