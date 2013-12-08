082852b2753e3ddf207659f5192a9e7a5c83deb7
Michael Schmuki <michischmuki@gmail.com>
2013-12-08 12:58:24 +0100

    shiny new icons

--- src/main/java/ch/kampfnacht/WebConfiguration.java
+++ src/main/java/ch/kampfnacht/WebConfiguration.java
@@ -11,11 +11,15 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		//static "webroot"-files
+		// static "webroot"-files
 		registry.addResourceHandler("/robots.txt").addResourceLocations("/robots.txt");
-		//TODO: add favicon
-		
-		//webapp
+		registry.addResourceHandler("/favicon.png").addResourceLocations("/favicon.png");
+		registry.addResourceHandler("/favicon.ico").addResourceLocations("/favicon.ico");
+		registry.addResourceHandler("/apple-touch-icon-144x144-precomposed.png").addResourceLocations("/apple-touch-icon-144x144-precomposed.png");
+		registry.addResourceHandler("/apple-touch-icon-57x57-precomposed.png").addResourceLocations("/apple-touch-icon-57x57-precomposed.png");
+		registry.addResourceHandler("/apple-touch-icon-72x72-precomposed.png").addResourceLocations("/apple-touch-icon-72x72-precomposed.png");
+		registry.addResourceHandler("/apple-touch-icon-precomposed.png").addResourceLocations("/apple-touch-icon-precomposed.png");
+		// webapp (brunch build)
 		registry.addResourceHandler("/app/**").addResourceLocations("/app/");
 		registry.addResourceHandler("/app").addResourceLocations("/app/index.html");
 	}
